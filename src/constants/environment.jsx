90e379ca917e1805867699ef42b39cbba8d187ab
007AxelBerkati <bucel1916@gmail.com>
2022-11-14 14:11:00 +0700

    add env

---
+++ src/constants/environment.jsx
@@ -0,0 +1,19 @@
+// export const firebaseApiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+// export const firebaseAuthDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+// export const firebaseProjectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+// export const firebaseStorageBucket =
+//   process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+// export const firebaseMessagingSenderId =
+//   process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
+// export const firebaseFirebaseAppId = process.env.REACT_APP_FIREBASE_APP_ID;
+// export const firebaseRealtimeDatabase = process.env.REACT_APP_FIREBASE_RDB;
+
+export const firebaseApiKey = import.meta.env.VITE_APP_FIREBASE_API_KEY;
+export const firebaseAuthDomain = import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN;
+export const firebaseProjectId = import.meta.env.VITE_APP_FIREBASE_PROJECT_ID;
+export const firebaseStorageBucket = import.meta.env
+  .VITE_APP_FIREBASE_STORAGE_BUCKET;
+export const firebaseMessagingSenderId = import.meta.env
+  .VITE_APP_FIREBASE_MESSAGING_SENDER_ID;
+export const firebaseFirebaseAppId = import.meta.env.VITE_APP_FIREBASE_APP_ID;
+export const firebaseRealtimeDatabase = import.meta.env.VITE_APP_FIREBASE_RDB;
